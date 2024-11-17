36d6140b12efab96c62d1668d7e22c0ac3a190a3
Mohd Shadab <ms.shadab06@gmail.com>
2024-11-17 12:53:59 +0530

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-import { UserButton, useUser } from "@clerk/clerk-react";
+import { SignInButton, UserButton, useUser } from "@clerk/clerk-react";
 import React from "react";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
 
 function Header() {
   const { user, isSignedIn } = useUser();
-
   return (
     <div className="flex justify-between items-center shadow-sm p-5">
-      <img src="/logo.svg" width={150} height={100} />
-
-      <ul className="hidden md:flex gap-16">
+      <Link to={"/"}>
+        <img src="/logo.svg" width={150} height={100} />
+      </Link>
+      <ul className="hidden  md:flex gap-16">
         <li className="font-medium hover:scale-105 transition-all cursor-pointer hover:text-primary">
           Home
         </li>
@@ -33,7 +33,9 @@ function Header() {
           </Link>
         </div>
       ) : (
-        <Button>Submit Listing</Button>
+        <SignInButton mode="modal" fallbackRedirectUrl="/profile">
+          <Button>Submit Listing</Button>
+        </SignInButton>
       )}
     </div>
   );
